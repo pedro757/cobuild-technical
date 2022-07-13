a3de2c55452ed1555d3e0c3eee14928ccf663c3b
Pedro Sanchez <pedro13jzc@gmail.com>
2022-07-13 14:01:52 -0400

    feat: add update funtionality

---
+++ src/pages/task/[id].tsx
@@ -0,0 +1,102 @@
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import TextField from "@mui/material/TextField";
+import { Task } from "@prisma/client";
+import type { NextPage } from "next";
+import { useSession } from "next-auth/react";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { ChangeEvent, FormEvent, useState } from "react";
+import ButtonAppBar from "../../components/navbar";
+import { trpc } from "../../utils/trpc";
+
+const EditTask: NextPage = () => {
+  const { data: session } = useSession();
+  const [form, setForm] = useState<Task>({ id: "", name: "", content: "", done: false });
+  const router = useRouter();
+  const { id } = router.query;
+  if (id && typeof id === 'string') {
+    trpc.useQuery(
+      [
+        "task.byId",
+        {
+          id: id
+        },
+      ],
+      {
+        onSuccess(data) {
+          if (data != null) {
+            setForm(data)
+          }
+        },
+      }
+    );
+  }
+  const mutation = trpc.useMutation("task.update", {
+    onSuccess() {
+      router.push("/");
+    },
+  });
+
+  if (!session) {
+    return (
+      <>
+        <ButtonAppBar />
+        <Container maxWidth="md">
+          <div>Please Log In</div>
+        </Container>
+      </>
+    );
+  }
+
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    mutation.mutate(form);
+  }
+
+  function handleChange(
+    event: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+  ) {
+    const { name, value } = event.target;
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  }
+  return (
+    <>
+      <Head>
+        <title>Edit Task</title>
+      </Head>
+      <ButtonAppBar />
+      <Container maxWidth="md">
+        <form onSubmit={handleSubmit}>
+          <TextField
+            required
+            margin="normal"
+            id="nameInput"
+            label="Name"
+            name="name"
+            value={form.name}
+            onChange={(e) => handleChange(e)}
+          />
+          <TextField
+            required
+            fullWidth
+            margin="normal"
+            id="contentInput"
+            label="Content"
+            name="content"
+            value={form.content}
+            onChange={(e) => handleChange(e)}
+          />
+          <Button variant="contained" color="primary" type="submit">
+            Submit
+          </Button>
+        </form>
+      </Container>
+    </>
+  );
+};
+
+export default EditTask;
--- src/server/router/task.ts
+++ src/server/router/task.ts
@@ -7,6 +7,30 @@ export const taskRouter = createRouter()
       return await ctx.prisma.task.findMany();
     },
   })
+  .query("byId", {
+    input: z.object({
+      id: z.string(),
+    }),
+    async resolve({ ctx, input }) {
+      return await ctx.prisma.task.findUnique({
+        where: { id: input.id },
+      });
+    },
+  })
+  .mutation("update", {
+    input: z.object({
+      id: z.string().optional(),
+      name: z.string(),
+      content: z.string(),
+      done: z.boolean().optional(),
+    }),
+    async resolve({ ctx, input }) {
+      return await ctx.prisma.task.update({
+        where: { id: input.id },
+        data: input,
+      });
+    },
+  })
   .mutation("create", {
     input: z.object({
       name: z.string(),
