b9de5b1cf6a0610fa5dacadb7ec8a831f52fe03e
Pedro Sanchez <pedro13jzc@gmail.com>
2022-07-13 13:59:31 -0400

    fix: correct link path

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -28,7 +28,7 @@ export default function ButtonAppBar() {
           </Typography>
           {session &&
             <Typography variant="h6" component="div">
-              <Link href="/createTask">
+              <Link href="/task/create">
                 <div style={{ display: "flex", cursor: "pointer" }}>
                   <AddIcon sx={{ marginRight: 1, marginTop: "4px" }} />
                   create
